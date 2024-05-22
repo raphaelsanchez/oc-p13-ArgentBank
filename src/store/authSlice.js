091c90e390b0c57581e71fc004e87be4426c91e2
Raphael Sanchez <hello@raphaelsanchez.design>
2024-05-22 19:18:45 +0200

    hotfix: Remove console.log statement in updateUserProfile function

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -63,7 +63,6 @@ export const updateUserProfile = createAsyncThunk(
             const response = await api.put('/user/profile', userUpdates, {
                 headers: { Authorization: `Bearer ${token}` },
             })
-            console.log(response.data)
             return response.data
         } catch (error) {
             if (error.response && error.response.data) {
